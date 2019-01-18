7db382240796f68804a6cb1a079cc8af8c88f8af
Andrey Odintsov <forestryks1@gmail.com>
2019-01-18 17:07:59 +0000

    setup.sh will not download source archive if already exists

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.88.tar.xz
+if [ ! -f linux-4.14.88.tar.xz ]; then
+    wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.88.tar.xz
+fi
 tar -xf linux-4.14.88.tar.xz
 cp linux-4.14.88 linux-4.14.88-kjudge -r
 cd linux-4.14.88-kjudge
